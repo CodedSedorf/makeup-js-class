a434948d453f07172b0134e6b30b64e0cf17320e
Dev Sedorf <sedorfdlc@gmail.com>
2026-02-20 03:32:19 -0800

    first commit

---
+++ operator.js
@@ -0,0 +1,31 @@
+// let age1 = 50;
+// let age2 = 72;
+// let num1 = 9;
+// let num2 = 3;
+// console.log(age1 + age2);
+// console.log(age1 - age2);
+// console.log(age1 * age2);
+// console.log(age1 / age2);
+// console.log(num1 ** num2);
+// console.log(num1 % num2);
+
+// let x = 5;
+// console.log(x);
+
+// x += 3; //x = x + 3
+// console.log(x);
+
+// x -= 20 // x = x - 20
+// console.log(x);
+
+
+// x *= 10; //x = x * 10
+// console.log(x);
+
+// x /= 5
+
+let y = 24;
+console.log(++y);
+console.log(--y);
+
+
